ab56e654051441f0d4f3d0c654cbf5846945c444
maxra <max.raadgever@adyen.com>
2026-01-30 12:03:37 +0100

    Hi bhavesh

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './home/Home.js';
 import Signup from './signup/Signup.js';
+import Backoffice from './backoffice/Backoffice.js';
 import Dashboard from './dashboard/Dashboard.js';
 import Products from './dashboard/Products.js';
 import Transactions from './dashboard/Transactions.js';
@@ -17,6 +18,7 @@ function App() {
          <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/backoffice" element={<Backoffice />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
           <Route exact path="/products" element={<Products />} />
---
+++ react-app/src/backoffice/Backoffice.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Link from '@mui/material/Link';
+
+import { useNavigate } from 'react-router-dom';
+
+import Footer from "../layout/Footer.js";
+import SmallBanner from "../layout/SmallBanner.js";
+import ErrorMessage from "../util/ErrorMessage.js";
+
+function Backoffice() {
+
+    return (
+
+        <div>
+
+        <Container maxWidth="sm">
+                <h1> HI BHAVESH </h1>
+          </Container>
+           <Footer/>
+            </div>
+
+
+  );
+}
+
+
+export default Backoffice;
